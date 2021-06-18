2c67540db85b6bcedbffedbd7710fd216000a904
belledon <mbelledonne@gmail.com>
2021-06-17 22:02:11 -0400

    checkpoint

--- src/inference/procedures/tracker_inv.jl
+++ src/inference/procedures/tracker_inv.jl
@@ -10,14 +10,22 @@ export split_merge_proposal, split_merge_involution
 
     # refine or coarsen?
     if ({:refine} ~ bernoulli(w))
+        state = trace[:trackers => tracker => :state]
+        n = prod(size(state))
+        mu = mean(state)
+        w_min = {:w_min} ~ uniform(0., mu)
+        w_max = {:w_max} ~ uniform(mu, 1.0)
+        bounds = [w_min, mu, w_max]
+        args = (bounds, [0.5, 0.5])
+        {:deltas} ~ broadcasted_piecewise_uniform()
         dims = level_dims(params, lvl + 1)
-        median = {:median} ~ uniform(0., 1.0)
+        window = {:window} ~ uniform(0., 1.0)
         # 8
         k = prod(dims) - prod(level_dims(params, lvl))
-        w = k % 2 == 0 ? k / 2 : (k - 1) / 2
+        w = Int64(k % 2 == 0 ? k / 2 : (k - 1) / 2)
 
-        {:lower} ~ broadcasted_normal(fill((epsilon, median - epsilon), w))
-        {:upper} ~ broadcasted_normal(fill((median + epsilon, 1.0 - epsilon), w))
+        {:lower} ~ broadcasted_uniform(fill((epsilon, median - epsilon), w))
+        {:upper} ~ broadcasted_uniform(fill((median + epsilon, 1.0 - epsilon), w))
     end
 end
 
@@ -45,31 +53,9 @@ epsilon = 1E-10
         upper = @read(u[:upper], :continuous)
         dims = level_dims(params, next_lvl)
         n = prod(dims)
-        k = n - 2 * length(lower)
-        if k == 1
-            next_state = [lower; median; upper]
-        else
-            next_state = [lower; median; median; upper]
-        end
-        # sd = sum(deltas)
-        # ms = mean(state)
-        # diff = (ms * n) - sd
-        # if diff >= 1.0
-        #     println("d > 1")
-        #     deltas ./= sd
-        #     deltas .*= (ms * (n-k) - epsilon)
-        # else
-        #     println("d < 0")
-        #     deltas ./= sd
-        #     deltas .*= (ms * (n - k) - epsilon)
-        # end
-        # x0 = ms * n - sum(deltas)
-        # x0 = fill(x0 ./ k, k)
-        # # 8x1 -> 3x3
-        # next_state = reshape([deltas; x0], dims)
-        @show mean(next_state)
-        # # mean is now correct but could be out of range
-        # next_state = clean_state(next_state)
+        nmed = n - (length(lower) + length(upper))
+        next_state = [lower; fill(median, nmed); upper]
+        next_state = reshape(next_state, dims)
         @write(u_prime[:refine], false, :discrete)
         # update t_prime
         @write(t_prime[:trackers => tracker => :level],
@@ -83,11 +69,14 @@ epsilon = 1E-10
         next_state = coarsen_state(params, state, next_lvl)
         @write(u_prime[:refine], true, :discrete)
         k = prod(size(next_state))
-        #    27 -> 8
         @show size(state)
         @show size(next_state)
-        @write(u_prime[:deltas], state[1:end - k], :continuous)
-        # 36 -> 9 -> 1
+        med = Statistics.median(state)
+        lower = state[state .< median]
+        upper = state[state .> median]
+        @write(u_prime[:median], med, :continuous)
+        @write(u_prime[:lower], lower, :continuous)
+        @write(u_prime[:upper], upper, :continuous)
         @write(t_prime[:trackers => tracker => :level],
                 next_lvl, :discrete)
         @write(t_prime[:trackers => tracker => :state],
