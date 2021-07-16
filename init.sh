7ec61b4ef78eb0def0ac55e1c8ec1f117952c6c3
belledon <mbelledonne@gmail.com>
2021-07-16 18:33:31 -0400

    clarified template names

--- init.sh
+++ init.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-
 sconfig_dir=$(realpath "$0" | xargs dirname)
 project_dir="$1"
 
+echo "(  ) Initializing config files"
 if [ -z "$project_dir" -a "$project_dir" != " " ]; then
     project_dir=$(dirname "${sconfig_dir}")
-    echo "No project directory provided, using ${project_dir}"
+    printf "\tNo project directory provided, using %s" "${project_dir}"
 fi
 
-echo "Initializing config files"
-cp "$sconfig_dir/template_run.sh" "$project_dir/run.sh"
-cp "$sconfig_dir/template_setup.sh" "$project_dir/setup.sh"
-cp "$sconfig_dir/template_senv.conf" "$project_dir/default.conf"
-cp "$sconfig_dir/load_config.sh" "$project_dir/load_config.sh"
+cp "${sconfig_dir}/templates/*.sh" "${project_dir}/"
+cp "${sconfig_dir}/templates/*.conf" "${project_dir}/"
+printf "(\xE2\x9C\x94) Initializing config files\n"
 
 # chmod +x "$project_dir/*.sh"
