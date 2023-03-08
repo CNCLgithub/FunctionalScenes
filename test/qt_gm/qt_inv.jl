3d98f3d550fcf0605aec5728de83dc78431debfb
belledon <mbelledonne@gmail.com>
2023-03-08 09:12:22 -0500

    testing `merge` moves

--- test/qt_gm/qt_inv.jl
+++ test/qt_gm/qt_inv.jl
@@ -74,10 +74,12 @@ function mytest()
     display(start_node)
 
     cm = choicemap()
+    # root node has children
     cm[(1, Val(:production)) => :produce] = true
-    cm[(3, Val(:aggregation)) => :mu] = 0.1
     for i = 1:4
+        # only one child of root has children
         cm[(i+1, Val(:production)) => :produce] = i == 1
+        # child of 2 should not reproduce
         cm[(Gen.get_child(2, i, 4), Val(:production)) => :produce] = false
     end
     (trace, ls) = Gen.generate(quad_tree_prior, (start_node, 1), cm)
@@ -86,6 +88,7 @@ function mytest()
     translator = SymmetricTraceTranslator(qt_split_merge_proposal,
                                           (node,),
                                           qt_sm_inv_manual)
+    (new_trace, w) = translator(trace)
 
 
 
